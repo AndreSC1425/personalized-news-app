00efe67f2c7c34215f86407942717e1389c356d0
AndreSC1425 <andreh@alumni.usc.edu>
2025-06-29 12:23:56 -0400

    Update SecurityConfig.java

--- backend/src/main/java/com/newsapp/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/newsapp/backend/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://personalized-news-o1rrtvjou-andresc1425s-projects.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
