fce0ce2ad09f99fc15f93baaf408ef2eea73ca97
AndreSC1425 <andreh@alumni.usc.edu>
2025-06-29 12:23:19 -0400

    Update WebConfig.java

--- backend/src/main/java/com/newsapp/backend/config/WebConfig.java
+++ backend/src/main/java/com/newsapp/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://personalized-news-o1rrtvjou-andresc1425s-projects.vercel.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
